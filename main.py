a0679599d2757d3c2176d54f64d085ba4b6f7178
Taha <tahanaeem12@hotmail.com>
2020-08-19 10:28:01 -0400

    seperated scrape function to only parse html, requesting is done elsewhere

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-import requests
 from lxml import *
 from lxml import etree
 import io
 import time
+import requests
 
 # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+
+def scrapeSite(htmlDoc):
+
+    # Parses the Bytes and returns a ElementTree object
+    htmlTree = etree.parse(io.BytesIO(htmlDoc), etree.HTMLParser(encoding="utf-8"))
+
+    # Note: Xpath is a language for navigating through a XML file structure
+
+    # Querys HTML tree for <a> tags containing keywords
+    result = htmlTree.xpath(
+        "//a[contains(@href,'scholarship') or contains(@href,'bursar')]"
+    )
+
+    # Direct link is found
+    if result:
+        # finds first URL that is accessible
+        for element in result:
+            url = element.attrib["href"]
+            if doesWebsiteExist(url):
+                return url
+    # No accessible URLs were found
+    return None
 
 
 def findALink(url):
@@ -12,10 +35,11 @@ def findALink(url):
 
     try:
         # HTML DOCUMENT in Bytes
-        # start = time.perf_counter()
-        htmlDoc = requests.get(url, headers=headers).content
-        # end = time.perf_counter()
-        # print(end - start)
+        start = time.perf_counter()
+        r = requests.get(url, headers=headers)
+        htmlDoc = r.content
+        end = time.perf_counter()
+        print(f"{end - start} {url}")
 
         # Parses the Bytes and returns a ElementTree object
         htmlTree = etree.parse(io.BytesIO(htmlDoc), etree.HTMLParser())
@@ -32,7 +56,6 @@ def findALink(url):
             # finds first URL that is accessible
             for element in result:
                 url = element.attrib["href"]
-                print(url)
                 if doesWebsiteExist(url):
                     return url
         # No accessible URLs were found
