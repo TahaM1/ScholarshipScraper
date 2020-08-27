5cd51f4455ed07645451dcd80060468c522f66e1
Taha <tahanaeem12@hotmail.com>
2020-08-27 19:55:07 -0400

    react build files

---
+++ react-frontend/build/precache-manifest.0c6f26d89448e678cf690fd4af62e899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13143d7c81fc67a5f2fec03fa4a9d8d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c6ff244221f8e99eaa0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f1de92157a9a94daf754",
+    "url": "/static/js/2.6186c8b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6186c8b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6ff244221f8e99eaa0",
+    "url": "/static/js/main.9f438d0a.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
