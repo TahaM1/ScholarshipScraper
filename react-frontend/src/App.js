05a535527373fa55b4638b194efd3f30f80ca9a7
Taha <tahanaeem12@hotmail.com>
2020-08-28 19:28:25 -0400

    minor tweaks

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Form from "./Form";
+import Form from "./components/Form";
 
 class App extends React.Component {
   state = {
     links: [],
+    isFetching: false,
   };
 
   updateLinks = (data) => {
@@ -16,18 +17,26 @@ class App extends React.Component {
   };
 
   render() {
+    let message;
+
+    if (this.state.links.length > 0) {
+      message = this.state.links.map((link, i) => {
+        return (
+          <div key={`link-${i}`}>
+            <a href={link}>{link}</a>
+          </div>
+        );
+      });
+    } else {
+      message = <div>Api hasn't been called. Submit the form above.</div>;
+    }
+
     return (
       <div className="App">
         <div>
           <Form updateResponse={this.updateLinks} />
         </div>
-        <div>
-          {this.state.links.length > 0 ? (
-            <div>{this.state.links}</div>
-          ) : (
-            <div>Api has not been fetched</div>
-          )}
-        </div>
+        <div>{message}</div>
       </div>
     );
   }
