2919979776d682071d910511ca300e18a1be1495
Taha <tahanaeem12@hotmail.com>
2020-08-30 17:15:15 -0400

    added title

---
+++ react-frontend/src/components/Title.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Typography, Box, makeStyles } from "@material-ui/core";
+import "fontsource-roboto";
+
+export class Title extends Component {
+  render() {
+    return (
+      <Typography component="div" variant="h2">
+        <Box marginTop={10} p={0}>
+          ScholarshipScraper
+        </Box>
+      </Typography>
+    );
+  }
+}
+
+export default Title;
